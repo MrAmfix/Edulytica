c1c80fc9c6a81e9fca7d7226cc14506f6b6c4765
MrAmfix <nat.rud.ufa@gmail.com>
2025-03-20 00:28:27 +0300

    FOR

--- tests/data_handling/test_vkr_parser.py
+++ tests/data_handling/test_vkr_parser.py
@@ -23,8 +23,7 @@ class TestParserVKR(unittest.TestCase):
 
         # Проверка вызова requests.get
         self.assertTrue(mock_requests_get.called)
-        # 1 раз для каждой персоны + 1 раз для файла
-        self.assertEqual(mock_requests_get.call_count, 4)
+        self.assertEqual(mock_requests_get.call_count, 4)  # 1 раз для каждой персоны + 1 раз для файла
 
     @patch('Edulytica.src.data_handling.VKRParser.requests.get')
     @patch('Edulytica.src.data_handling.VKRParser.open', new_callable=mock_open, read_data='790')
