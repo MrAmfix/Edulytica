d4c35c71fea7a0fab17da4d29edcdb218d1df3e6
MrAmfix <nat.rud.ufa@gmail.com>
2025-03-20 00:08:20 +0300

    Pop

---
+++ recommit.sh
@@ -0,0 +1,9 @@
+git add .
+git commit -m "Prikov Development"
+git push origin development
+git checkout E-58-2
+git pull
+git checkout origin/development -- .
+git add .
+git commit -m "Prikov E-58-2"
+git push origin E-58-2
