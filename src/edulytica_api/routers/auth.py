cde73705887f5b376b89863855c8f6a47d0361b4
MrAmfix <nat.rud.ufa@gmail.com>
2025-03-20 00:04:50 +0300

    Prikov E-58-2

--- src/edulytica_api/routers/auth.py
+++ src/edulytica_api/routers/auth.py
@@ -29,9 +29,7 @@ async def login(
 ):
     user = await UserCrud.get_filtered_by_params(session=session, username=form_data.username)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Incorrect email or password")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
 
     user = user[0]
     password_validate(form_data.password, user.password)
@@ -104,9 +102,7 @@ async def refresh_token(
         refresh_token=token
     )
     if len(tokens) != 1:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Exception in token validation")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Exception in token validation")
 
     token = tokens[0]
     access_token = create_access_token(user.id)
@@ -147,9 +143,7 @@ async def logout(
         refresh_token=token
     )
     if len(tokens) != 1:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Exception in token validation")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Exception in token validation")
 
     token = tokens[0]
     await TokenCrud.delete(session=session, record_id=token.id)
