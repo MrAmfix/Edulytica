11609f6d8b3c430b09fb2aecddd2a098d001b3a2
MrAmfix <nat.rud.ufa@gmail.com>
2025-03-19 23:12:18 +0300

    Test

--- src/edulytica_api/database.py
+++ src/edulytica_api/database.py
@@ -6,7 +6,7 @@ import os
 from typing import Generator
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession, create_async_engine
-
+#zzzz
 
 load_dotenv()
 DATABASE_URL = f'postgresql+asyncpg://{os.environ.get("POSTGRES_USER")}:{os.environ.get("POSTGRES_PASSWORD")}@{os.environ.get("POSTGRES_IP")}:{os.environ.get("POSTGRES_PORT")}/{os.environ.get("POSTGRES_DB")}'
